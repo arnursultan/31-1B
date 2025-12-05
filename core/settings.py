bcedb4345a23e44087d91ef9dea2ea33d131ac42
arnursultan <badasspubgm@gmail.com>
2025-12-05 21:22:13 +0600

    lesson 7 Swagger, Authorization, Permissions

--- core/settings.py
+++ core/settings.py
@@ -13,6 +13,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -22,9 +23,11 @@ INSTALLED_APPS = [
     'django_filters',
 
     'rest_framework',
-    
+    'rest_framework.authtoken',
+
+    'drf_yasg',
+
     'app.product',
-    
 ]
 
 MIDDLEWARE = [
@@ -36,6 +39,49 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+JAZZMIN_SETTINGS = {
+    "site_title": "Administration panel",
+    "site_header": "Library",
+    "site_brand": "Server 31-1B",
+    "welcome_sign": "Welcome to the Server 31-1B admin panel",
+    "copyright": "Geeks",
+    "search_model": ["auth.User", "auth.Group"],
+    "topmenu_links": [
+        {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
+        {"name": "Support", "url": "https://t.me/ar_nursultan", "new_window": True},
+        {"model": "auth.User"},
+    ],
+    "show_sidebar": True,
+    "changeform_format": "horizontal_tabs",
+}
+
+JAZZMIN_UI_TWEAKS = {
+    #  Light themes
+    # "theme" : "flatly",
+    # "theme" : "simplex",
+    # "theme" : "cerulean",
+    # "theme" : "cosmo",
+    # "theme" : "journal",
+    # "theme" : "litera",
+    # "theme" : "lumen",
+    # "theme" : "lux",
+    # "theme" : "materia",
+    # "theme" : "minty",
+    # "theme" : "pulse",
+    # "theme" : "sandstone",
+    # "theme" : "simplex",
+    # "theme" : "sketchy",
+    # "theme" : "spacelab",
+    "theme" : "united",
+    # "theme" : "yeti",
+    #  Dark themes
+    # "theme": "darkly",
+    # "theme" : "slate",
+    # "theme" : "cyborg",
+    # "theme" : "solar",
+    # "theme" : "superhero",
+}
 
 ROOT_URLCONF = 'core.urls'
 
@@ -102,5 +148,32 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
     ]
+}
+
+SWAGGER_SETTINGS = {
+    'USE_SESSION_AUTH': True,
+    'JSON_EDITOR': True,
+    'DEFAULT_MODEL_RENDERING': 'example',
+
+    'SECURITY_DEFINITIONS': {
+        'Token': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header',
+            'description': 'Введите токен в формате: Token <your token>'
+        }
+    },
+
+    'REFETCH_SCHEMA_WITH_AUTH': True,
+    'REFETCH_SCHEMA_ON_LOGOUT': True,
 }
