4236ea3a6b6b145562b35c2ee5145e22f7955540
arnursultan <badasspubgm@gmail.com>
2025-12-19 19:22:55 +0600

    lesson 2 Permissions

--- users/serializers.py
+++ users/serializers.py
@@ -1,18 +1,40 @@
 from rest_framework import serializers
-from .models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class UserRegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(
+        write_only=True,
+        min_length=8
+    )
 
     class Meta:
         model = User
-        fields = ('id', 'email', 'username', 'password')
+        fields = (
+            'id',
+            'email',
+            'username',
+            'password'
+        )
 
     def create(self, validated_data):
-        user = User(
+        return User.objects.create_user(
             email=validated_data['email'],
-            username=validated_data['username'],
+            password=validated_data['password'],
+            phone=validated_data('phone')
         )
-        user.set_password(validated_data['password'])
-        user.save()
         return user
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'email',
+            'email',
+            'phone',
+            'is_active',
+            'is_staff',
+            'data_joined'
+        )
--- users/views.py
+++ users/views.py
@@ -1,19 +1,36 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-from .serializers import UserRegisterSerializer
+from .serializers import (
+    UserRegisterSerializer,
+    UserProfileSerializer
+)
+
 
 class RegisterAPIView(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
         serializer = UserRegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                {"message": "Пользователь создан!"},
-                status=status.HTTP_201_CREATED
-            )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
         return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
+            {
+                "message": "Пользователь успешно зарегистрирован",
+                "user": {
+                    "id": user.id,
+                    "email": user.email,
+                }
+            },
+            status=status.HTTP_201_CREATED
         )
+
+class UserProfileAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request):
+        serializer = UserProfileSerializer(request.user)
+        return Response(serializer.data)
